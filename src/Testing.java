9223bba7123ffcbfdc9d210508830da38293ad43
Kathir Dev <121499256+Kathir-D@users.noreply.github.com>
2024-11-04 14:54:20 -0600

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -1,20 +1,8 @@
 public class Testing
 {
-    	public static void main( String[] args ) {
-
-            int a = 3;
-            int b = 2;
-
-
-            if (a < b || a + b > 3 && 313 > a++) {
-                System.out.println(a);
-                System.out.println(b);
-            }
-            System.out.println("yes");
-            System.out.println(a);
-                System.out.println(b);
-
-        }
+    public static void main( String[] args ) {
+        
+    }
 }
 
 
