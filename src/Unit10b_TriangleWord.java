65a5f8f3eb356321f2495d3649fa7fed6fac876b
Kathir Dev <121499256+Kathir-D@users.noreply.github.com>
2024-11-12 09:54:27 -0600

    like idk unit 10 day 2 not finished

---
+++ src/Unit10b_TriangleWord.java
@@ -0,0 +1,83 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Unit10b_TriangleWord {
+	private String word;
+
+	public Unit10b_TriangleWord(String w) {
+		word = w;
+	}
+
+	public String toString() {
+		
+		StringBuilder output = new StringBuilder();
+		for (int i = 0; i < word.length(); i++) {
+			for (int j = 0; j < word.length() - i - 1; j++) {
+				output.append(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				output.append(word.charAt(j));
+				if (j < i) {
+					output.append(" ");
+				}
+			}
+			for (int j = i - 1; j >= 0; j--) {
+				output.append(word.charAt(j));
+			}
+			output.append("\n");
+		}
+		return output.toString();
+	}
+}
+/*
+
+Page
+1
+of 2
+© A+ Computer Science – Nested Loops - www.apluscompsci.com
+Lab Goal : This lab was designed to teach you how to use nested loops. You will also learn how to make
+some cool shapes.
+Lab Description : Use nested loops to print out the triangle word pattern show below.
+Sample Data :
+WOW
+BEN
+SAMMY
+WILE
+APLUSCOMPSCI
+Sample Output :
+W
+O O
+WOWOW
+B
+E E
+NEBEN
+S
+A A
+M M
+M M
+YMMASAMMY
+W
+I I
+L L
+ELIWILE
+A
+P P
+L L
+U U
+S S
+C C
+O O
+M M
+P P
+S S
+C C
+ICSPMOCSULPAPLUSCOMPSCI
+Files Needed ::
+TriangleWord.java
+TriangleWordRunner.java
+A+ Computer Science TRIANGLE
+WORDS
+© A+ Computer Science – Nested Loops - www.apluscompsci.com
+
+ */
---
+++ src/Unit10b_TriangleWordRunner.java
@@ -0,0 +1,77 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+public class Unit10b_TriangleWordRunner
+{
+   public static void main(String args[])
+   {
+       Unit10b_TriangleWord word1 = new Unit10b_TriangleWord("WOW");
+       System.out.println(word1);
+
+       Unit10b_TriangleWord word2 = new Unit10b_TriangleWord("BEN");
+       System.out.println(word2);
+
+       Unit10b_TriangleWord word3 = new Unit10b_TriangleWord("SAMMY");
+       System.out.println(word3);
+
+       Unit10b_TriangleWord word4 = new Unit10b_TriangleWord("WILE");
+       System.out.println(word4);
+
+       Unit10b_TriangleWord word5 = new Unit10b_TriangleWord("APLUSCOMPSCI");
+       System.out.println(word5);
+
+	}
+}
+
+/*
+
+Page
+1
+of 2
+© A+ Computer Science – Nested Loops - www.apluscompsci.com
+Lab Goal : This lab was designed to teach you how to use nested loops. You will also learn how to make
+some cool shapes.
+Lab Description : Use nested loops to print out the triangle word pattern show below.
+Sample Data :
+WOW
+BEN
+SAMMY
+WILE
+APLUSCOMPSCI
+Sample Output :
+  W
+ O O
+WOWOW
+  B
+ E E
+NEBEN
+    S
+   A A
+  M   M
+ M     M
+YMMASAMMY
+W
+I I
+L L
+ELIWILE
+A
+P P
+L L
+U U
+S S
+C C
+O O
+M M
+P P
+S S
+C C
+ICSPMOCSULPAPLUSCOMPSCI
+Files Needed ::
+TriangleWord.java
+TriangleWordRunner.java
+A+ Computer Science TRIANGLE
+WORDS
+© A+ Computer Science – Nested Loops - www.apluscompsci.com
+
+ */
