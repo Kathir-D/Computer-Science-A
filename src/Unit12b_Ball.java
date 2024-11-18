6d504bc5896b88f7f3cfdccaabd503d0b1046261
Kathir Dev <121499256+Kathir-D@users.noreply.github.com>
2024-11-18 15:21:50 -0600

    Unit 12 day 2 pt.1

---
+++ src/Unit12b_Ball.java
@@ -0,0 +1,18 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+//design your Ball class
+//write all code for your Ball class here
+//test your Ball class with the BallRunner
+
+//make a Ball class
+      //add needed instance variables
+      //TYPE and CAPACITY are constants
+
+
+class Unit12b_Ball {
+    public Unit12b_Ball(){
+
+    }
+}
