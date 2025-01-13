84eebd0cff60f5904a35be815ee31f42895f850c
Kathir Dev <121499256+Kathir-D@users.noreply.github.com>
2025-01-13 08:05:46 -0600

    frq practice

--- APCSA 24-25/Testing.java
+++ APCSA 24-25/Testing.java
@@ -1,21 +1,4 @@
 public class Testing {
 
-  public static void main(String[] args) {
-    for (int i = 0; i < 4; i++) {
-      System.out.print("x");
-    }
-
-    System.out.println();
-
-    int size = 4;
-    for (int i = 0; i < size; i++) {
-      for (int j = size - i; j > 1; j--) {
-        System.out.print(" ");
-      }
-      for (int j = 0; j <= i; j++) {
-        System.out.print("A");
-      }
-      System.out.println();
-    }
-  }
+  public static void main(String[] args) {}
 }
