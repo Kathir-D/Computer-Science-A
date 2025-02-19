a32fc7571916d44cd67c67483f6055606cfa5df8
Kathir Dev <121499256+Kathir-D@users.noreply.github.com>
2025-02-19 15:11:27 -0600

    Unit 17 day 2 pt.1

--- APCSA 24-25/Unit17a_Ground.java
+++ APCSA 24-25/Unit17a_Ground.java
@@ -4,8 +4,10 @@
 
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
 public class Unit17a_Ground {
 
@@ -14,13 +16,58 @@ public class Unit17a_Ground {
   public Unit17a_Ground() throws IOException {
     //point the tiles at new array list
 
+    tiles = new ArrayList<Image>();
+
     //add in each of the images for the grass.gif, gtoroad.gif, road.gif, rtograss.gif
 
+    Image grass = ImageIO.read(new File("APCSA 24-25/Unit17a_grass.gif"));
+    tiles.add(grass);
+    Image gtoroad = ImageIO.read(new File("APCSA 24-25/Unit17a_gtoroad.gif"));
+    tiles.add(gtoroad);
+    Image road = ImageIO.read(new File("APCSA 24-25/Unit17a_road.gif"));
+    tiles.add(road);
+    Image rtograss = ImageIO.read(new File("APCSA 24-25/Unit17a_rtograss.gif"));
+    tiles.add(rtograss);
     //tiles.add( ImageIO.read(new File("grass.gif")) );
 
   }
 
   public void drawGround(Graphics2D g) {
     //draw the ground on the screen
+
+    /*
+    Use nested loops and ifs to draw the different images. The tiles need to be drawn
+    every 50 pixels since they are 50 x 50 px image.
+    Use the y position to determine which tile to draw:
+    The top two tiles are grass (when y is 0 and 50)
+    The third tile is grass to road
+    The middle tiles are road
+    The second to last tile is road to grass
+    The bottom tile is grass
+     */
+    int x, y = 0;
+    int current = 0;
+
+    for (int j = 0; j < 12; j++) {
+      if (j == 0 || j == 1) {
+        current = 0;
+      } else if (j == 2) {
+        current = 1;
+      } else if (j > 2 && j < 9) {
+        current = 2;
+      } else if (j == 9) {
+        current = 3;
+      } else if (j == 10 || j == 11) {
+        current = 0;
+      }
+
+      x = 0;
+      for (int i = 0; i < 16; i++) {
+        g.drawImage(tiles.get(current), x, y, null);
+        x += 50;
+      }
+
+      y += 50;
+    }
   }
 }
--- APCSA 24-25/Unit17a_Person.java
+++ APCSA 24-25/Unit17a_Person.java
@@ -5,8 +5,11 @@
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import javax.imageio.ImageIO;
 
 public class Unit17a_Person {
 
@@ -16,9 +19,18 @@ public class Unit17a_Person {
   public Unit17a_Person(int x, int y) throws IOException {
     this.x = x;
     this.y = y;
+    current = 0;
     //point the array list at a new ArrayList
-
+    list = new ArrayList<Image>();
     //add all images to the list
+    Image front = ImageIO.read(new File("APCSA 24-25/Unit17a_dude.gif"));
+    list.add(front);
+    Image left = ImageIO.read(new File("APCSA 24-25/Unit17a_dudeL.gif"));
+    list.add(left);
+    Image right = ImageIO.read(new File("APCSA 24-25/Unit17a_dudeR.gif"));
+    list.add(right);
+    Image back = ImageIO.read(new File("APCSA 24-25/Unit17a_dudeU.gif"));
+    list.add(back);
   }
 
   // will return true if person collides with another object
@@ -29,12 +41,13 @@ public class Unit17a_Person {
   }
 
   public void drawPerson(Graphics2D g) {
-    //draw the current image on the screen
+    //draw the current image
+    g.drawImage(list.get(current), x, y, null);
   }
 
   public Image getCurrent() {
     //return the current image
-    return null;
+    return list.get(current);
   }
 
   public int getX() {
@@ -46,18 +59,26 @@ public class Unit17a_Person {
   }
 
   public void moveRight() {
+    x += 10;
+    current = 2;
     //finish
   }
 
   public void moveLeft() {
+    x -= 10;
+    current = 1;
     //finish
   }
 
   public void moveUp() {
+    y -= 10;
+    current = 3;
     //finish
   }
 
   public void moveDown() {
+    y += 10;
+    current = 0;
     //finish
   }
 }
