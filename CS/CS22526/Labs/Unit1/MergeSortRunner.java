5657efcbb541b413408fba083b7d92d63447b652
Kathir Dev <121499256+Kathir-D@users.noreply.github.com>
2025-08-28 12:45:22 -0500

    Formated file MergeSortRunner.java

--- CS/CS22526/Labs/Unit1/MergeSortRunner.java
+++ CS/CS22526/Labs/Unit1/MergeSortRunner.java
@@ -11,13 +11,13 @@ import java.util.Arrays;
 public class MergeSortRunner {
 
   public static void main(String args[]) {
-    Comparable[] arr1 = {9, 5, 3, 2};
+    Comparable[] arr1 = { 9, 5, 3, 2 };
     MergeSort.mergeSort(arr1);
 
-    Comparable[] arr2 = {19, 52, 3, 2, 7, 21};
+    Comparable[] arr2 = { 19, 52, 3, 2, 7, 21 };
     MergeSort.mergeSort(arr2);
 
-    Comparable[] arr3 = {68, 66, 11, 2, 42, 31};
+    Comparable[] arr3 = { 68, 66, 11, 2, 42, 31 };
     MergeSort.mergeSort(arr3);
   }
 }
